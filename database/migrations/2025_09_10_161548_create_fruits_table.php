9a9a5d9c6f4c045a7f4c0055d0601674314d4246
ENISSAYEPFEDU <yassinebsofien@gmailcom>
2025-09-10 23:53:55 +0700

    Eloquent Models Generated Fruit model with migration, factory, and seeder. Updated model fillable fields (name, Price, bio) and inserted test data via Tinker.

--- database/migrations/2025_09_10_161548_create_fruits_table.php
+++ database/migrations/2025_09_10_161548_create_fruits_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('todos', function (Blueprint $table) {
+        Schema::create('fruits', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('title');
-            $table->boolean('is_completed');
+            $table->string('name');
+            $table->integer('Price');
+            $table->text('bio');
         });
     }
 
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('todos');
+        Schema::dropIfExists('fruits');
     }
 };
