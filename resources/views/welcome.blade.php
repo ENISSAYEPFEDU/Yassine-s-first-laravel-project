0135522c2d1b38f1d55c1a1306acf47bb96c082b
ENISSAYEPFEDU <yassinebsofien@gmailcom>
2025-09-10 22:08:25 +0700

    Configured TailwindCSS (v4.1.13) with Vite and applied basic styling to welcome and layout views. Added navigation header with links to home and fruit creation.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,24 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fruits mart</title>
+
+
+    @vite('resources/css/app.css')
+
 </head>
-<body>
+
+<body class= "text-center px-8 py-12">
     <h1>Welcome ! </h1>
     <p>Click the button bellow to view list of fruits . </p>
-    <a href= "/Fruits" class="btn" >
+
+
+    <a href= "/Fruits" class="btn mt-4 inline-block ">
         Find fruits
     </a>
 </body>
+
 </html>
