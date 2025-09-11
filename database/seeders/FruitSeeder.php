86098c3a280716de199a932aec85e17d3aa46274
ENISSAYEPFEDU <yassinebsofien@gmailcom>
2025-09-11 13:45:41 +0700

    Add FruitSeeder and integrate it into DatabaseSeeder
    - Created FruitSeeder to populate the database with random fruits using FruitFactory
    - Configured DatabaseSeeder to call FruitSeeder automatically during seeding
    - Ran `php artisan migrate:fresh --seed` to reset the database and successfully generate 50 random fruits
    
    This ensures the database has realistic test data (not relevant lol) for development and testing.

--- database/seeders/FruitSeeder.php
+++ database/seeders/FruitSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Fruit;
 
 class FruitSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class FruitSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Fruit::factory()->count(50)->create();
     }
 }
